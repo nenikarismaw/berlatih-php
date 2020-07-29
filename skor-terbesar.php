c11c7dcadd8135f975ef127386bd9dcb5ee92a79
Neni Karismawati <karismawatineni@gmail.com>
2020-07-30 04:16:33 +0700

    skor terbesar

--- skor-terbesar.php
+++ skor-terbesar.php
@@ -1,58 +1,39 @@
 <?php
-function skor_terbesar($arr){
-//kode di sini
-}
-
-// TEST CASES
-$skor = [
-  [
-    "nama" => "Bobby",
-    "kelas" => "Laravel",
-    "nilai" => 78
-  ],
-  [
-    "nama" => "Regi",
-    "kelas" => "React Native",
-    "nilai" => 86
-  ],
-  [
-    "nama" => "Aghnat",
-    "kelas" => "Laravel",
-    "nilai" => 90
-  ],
-  [
-    "nama" => "Indra",
-    "kelas" => "React JS",
-    "nilai" => 85
-  ],
-  [
-    "nama" => "Yoga",
-    "kelas" => "React Native",
-    "nilai" => 77
-  ],
-];
-
-print_r(skor_terbesar($skor));
-/* OUTPUT
-  Array (
-    [Laravel] => Array
-              (
-                [nama] => Aghnat
-                [kelas] => Laravel
-                [nilai] => 90
-              )
-    [React Native] => Array
-                  (
-                    [nama] => Regi
-                    [kelas] => React Native
-                    [nilai] => 86
-                  )
-    [React JS] => Array
-                (
-                  [nama] => Indra
-                  [kelas] => React JS
-                  [nilai] => 85
-                )
-  )
-*/
+echo "Skor Terbesar<br>";
+ $skor = [
+        [
+            "nama" => "Bobby",
+            "kelas" => "Laravel",
+            "nilai" => 78
+        ],
+        [
+            "nama" => "Regi",
+            "kelas" => "React Native",
+            "nilai" => 86
+        ],
+        [
+            "nama" => "Aghnat",
+            "kelas" => "Laravel",
+            "nilai" => 90
+        ],
+        [
+            "nama" => "Indra",
+            "kelas" => "React JS",
+            "nilai" => 85
+        ],
+        [
+            "nama" => "Yoga",
+            "kelas" => "React Native",
+            "nilai" => 77
+        ],
+      ];
+ 
+  function cmp($a, $b) {
+        if ($a['nilai'] == $b['nilai']) {
+            return 0;
+        }
+        return ($a['nilai'] > $b['nilai']) ? -1 : 1;
+    }
+  uasort($skor, 'cmp');
+  print_r($skor); 
 ?>
