e7ec46f862ee2b8c72b50137977245fe72a2d2f5
Neni Karismawati <karismawatineni@gmail.com>
2020-07-29 23:13:06 +0700

    hitung mean

--- cari-mean.php
+++ cari-mean.php
@@ -2,9 +2,16 @@
 
 function cari_mean($arr){
 //kode di sini
+    print_r($arr);
+    echo " Hasil mean adalah ";
+    $count_array = count($arr);
+    $jumlah=0;
+    for($i=0; $i<count($arr); $i++){
+        $jumlah+=$arr[$i];
+    }
+    echo round($jumlah/$count_array)."<br>";
 }
 
-TEST CASE 
 echo cari_mean([1, 2, 3, 4, 5]); // 3
 echo cari_mean([3, 5, 7, 5, 3]); // 5
 echo cari_mean([6, 5, 4, 7, 3]); // 5
