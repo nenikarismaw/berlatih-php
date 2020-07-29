f9eec9e590cc46c963dc8a71863e145e67e10ca0
Neni Karismawati <karismawatineni@gmail.com>
2020-07-29 20:31:38 +0700

    papan catur

--- papan-catur.php
+++ papan-catur.php
@@ -2,15 +2,26 @@
 
 function papan_catur($angka) {
 // tulis kode di sini
-    for($i=0; $i<=$angka; $i++){
-        for($j=0; $j<=$angka; $j++){
-            
+    echo "Papan Catur ($angka):<br>";
+    for($i=0; $i<$angka; $i++){
+        for($j=0; $j<$angka*2-($i%2); $j++){
+            if($i%2==0 && $j%2==0){
+                echo "#";
+            }else if($i%2==0 && $j%2==1){
+                echo "&nbsp;";
+            }else if($i%2==1 && $j%2==0){
+                echo "&nbsp;";
+            }else if($i%2!==0 && $j%2!==0){
+                echo "#";
+            }
         }
+        echo "<br>";
     }
+    echo "<br>";
 }
 
 // TEST CASES
-echo papan_catur(4) 
+echo papan_catur(4);
 /*
 # # # #
  # # #
@@ -18,7 +29,7 @@ echo papan_catur(4)
  # # #
  */
 
-echo papan_catur(8)
+echo papan_catur(8);
 /* 
 # # # # # # # #
  # # # # # # # 
@@ -30,7 +41,7 @@ echo papan_catur(8)
  # # # # # # #
 */
 
-echo papan_catur(5) 
+echo papan_catur(5);
 /*
 # # # # #
  # # # #
